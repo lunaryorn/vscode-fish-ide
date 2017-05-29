5c7306889b48d758c87beb7de347c03e4bb7af4d
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-05-29 12:17:41 +0200

    Run fish_indent on the document

--- src/extension.ts
+++ src/extension.ts
@@ -15,8 +15,65 @@
 // You should have received a copy of the GNU General Public License
 // along with vscode-hlint.  If not, see <http://www.gnu.org/licenses/>.
 
+import { execFile } from "child_process";
 import * as vscode from "vscode";
-import { ExtensionContext } from "vscode";
+import {
+    CancellationToken,
+    ExtensionContext,
+    FormattingOptions,
+    Range,
+    TextDocument,
+    TextEdit,
+} from "vscode";
+
+/**
+ * Run fish_indent in the current workspace.
+ *
+ * @param input The input for fish_indent
+ * @return The output of fish_indent
+ */
+const indentFish = (input: string): Promise<string> => new Promise<string>(
+    (resolve, reject) => {
+        const cwd = vscode.workspace.rootPath || process.cwd();
+        const fishIndent = execFile("fish_indent", { cwd },
+            (error, stdout, stderr) => {
+                if (error) {
+                    // tslint:disable-next-line:max-line-length
+                    reject(new Error(
+                        `Failed to run fish_indent: ${error.message}, ${stderr}`));
+                } else {
+                    resolve(stdout);
+                }
+            });
+        fishIndent.stdin.end(input);
+    },
+);
+
+/**
+ * Provide edits to format a fish document.
+ *
+ * @param document The document to format
+ * @param _options Formatting options, currently unused
+ * @param token A token to cancel the operation
+ * @return A sequence of edits to format the current document
+ */
+const provideDocumentFormattingEdits =
+    async (
+        document: TextDocument,
+        _options: FormattingOptions,
+        token: CancellationToken,
+    ) => {
+        try {
+            const formatted = await indentFish(document.getText());
+            const rangeMax = document.validateRange(new Range(
+                0, 0, Number.MAX_VALUE, Number.MAX_VALUE));
+            const formattingEdit = TextEdit.replace(rangeMax, formatted);
+            return token.isCancellationRequested ? [] : [formattingEdit];
+        } catch (error) {
+            vscode.window.showErrorMessage(
+                `Failed to format document: ${error}`);
+        }
+    };
 
 /**
  * Activate this extension.
@@ -25,7 +82,10 @@ import { ExtensionContext } from "vscode";
  *
  * @param _context The context for this extension
  */
-export function activate(_context: ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(
+        vscode.languages.registerDocumentFormattingEditProvider(
+            "fish", { provideDocumentFormattingEdits }));
     vscode.window.showInputBox("Hello fish!");
     return;
 }
