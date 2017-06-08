487d85046681da90b20c7165384ce0f3b436ac9e
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-06-08 11:46:21 +0300

    Fix project name in license header

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,19 @@
 // Copyright (C) 2017 Sebastian Wiesner <swiesner@lunaryorn.com>
 //
-// This file is part of vscode-hlint.
+// This file is part of vscode-fish-ide.
 //
-// vscode-hlint is free software: you can redistribute it and/or modify
+// vscode-fish-ide is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// vscode-hlint is distributed in the hope that it will be useful,
+// vscode-fish-ide is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with vscode-hlint.  If not, see <http://www.gnu.org/licenses/>.
+// along with vscode-fish-ide.  If not, see <http://www.gnu.org/licenses/>.
 
 import { execFile } from "child_process";
 import { homedir } from "os";
