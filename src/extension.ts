c2a2b442a12b002f2dd2c2bf4eedf6b2fe5c990d
Sebastian Wiesner <sebastian@swsnr.de>
2017-11-24 19:02:56 +0100

    Update my mail address

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Sebastian Wiesner <swiesner@lunaryorn.com>
+// Copyright (c) 2017 Sebastian Wiesner <sebastian@swsnr.de>
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
